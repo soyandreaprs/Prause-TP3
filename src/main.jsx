fd1e68cd55ac0658456d072e30a4cbe361c5bea4
Andrea <soyandreaprs@gmail.com>
2025-11-07 22:00:12 -0300

    Creación de rutas con React Router DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material'
+import theme from './theme.js'
+import './index.css'
+import '@fontsource/dm-sans'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
 )
