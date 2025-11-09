65667b2cfa6d87a4c362e4902fc6be81fc4d280a
Andrea <soyandreaprs@gmail.com>
2025-11-09 20:31:59 -0300

    Unificación de estilos de Material UI

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,16 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    typography: { fontFamily: "DM Sans", fontSize: 10 },
-})
+  typography: { fontFamily: "DM Sans", fontSize: 10 },
+  MuiContainer: {
+    styleOverrides: {
+      root: {
+        paddingLeft: 0,
+        paddingRight: 0,
+        margin: 0,
+      },
+    },
+  },
+});
 
-export default theme
+export default theme;
