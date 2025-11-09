20803756ce2ee87b05884d76c35de47463609000
Andrea <soyandreaprs@gmail.com>
2025-11-09 20:34:13 -0300

    Configuración de enrutamiento con React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Cart from "./pages/Cart";
 import NavBar from "./components/NavBar/";
+import Error from "./components/Error";
 import { Box, Container } from "@mui/material";
 
-
 function App() {
-
   return (
-    <Box sx={{gap: 20}} >
+    <Box sx={{ mt: 10, mb: 10 }}>
       <NavBar />
-      <Container sx={{}} >
+      <Container sx={{ p: 0 }}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/detail" element={<Detail />} />
+          <Route path="/artwork/:id" element={<Detail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/error" element={<Error />} />
         </Routes>
       </Container>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import App from './App.jsx'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from '@mui/material'
-import theme from './theme.js'
-import './index.css'
-import '@fontsource/dm-sans'
+import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme.js";
+import "./index.css";
+import "@fontsource/dm-sans";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
