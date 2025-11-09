edb003f3563927ccdc67f1bf26c9056e2a497141
Andrea <soyandreaprs@gmail.com>
2025-11-09 20:30:07 -0300

    Obtención de datos de API pública

--- src/services/api-art.js
+++ src/services/api-art.js
@@ -2,24 +2,26 @@ import axios from "axios";
 
 const URL = "https://collectionapi.metmuseum.org/public/collection/v1";
 
-
-export const getArtwork = async (q = "painting", limit = 9) => {
-    try {
-        const response = await axios.get(`${URL}/search`, { params: { q, hasPrimaryImage: true, hasArtistDisplayName: true } });
-        const ids = response.data && response.data.objectIDs
-            ? response.data.objectIDs.slice(0, limit)
-            : [];
-        return ids;
-    } catch (error) {
-        throw new Error(error);
-    }
+export const getArtwork = async (q = "painting", limit = 12) => {
+  try {
+    const response = await axios.get(`${URL}/search`, {
+      params: { q, hasPrimaryImage: true, hasArtistDisplayName: true },
+    });
+    const ids =
+      response.data && response.data.objectIDs
+        ? response.data.objectIDs.slice(0, limit)
+        : [];
+    return ids;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 export const getArtworkByID = async (id) => {
-    try {
-        const response = await axios.get(`${URL}/objects/${id}`);
-        return response.data;
-    } catch (error) {
-        throw new Error(error);   
-    }
-}
+  try {
+    const response = await axios.get(`${URL}/objects/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
